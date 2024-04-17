d258eaf29a66e85aa98371bd91bf066be61910e6
shivhub09 <shivnagori2020@gmail.com>
2024-04-17 19:19:35 +0530

    routing successfull

--- client/src/Components/MainScreen/Components/MainScreen-Header/MainScreenNavbar.jsx
+++ client/src/Components/MainScreen/Components/MainScreen-Header/MainScreenNavbar.jsx
@@ -1,5 +1,7 @@
-import React from 'react'
-import './MainScreenNavbar.css'
+import React from 'react';
+import './MainScreenNavbar.css';
+import { Link } from 'react-router-dom';
+
 const MainScreenNavbar = () => {
   return (
     <div className="MainScreenNavbar">
@@ -13,7 +15,7 @@ const MainScreenNavbar = () => {
                 <li className="item">About Us</li>
                 <li className="item">Services</li>
                 <li className="item">Contact Us</li>
-                <li className="item">Login</li>
+                <li className="item"> <Link to="/login">Login</Link></li>
             </ul>
         </div>
     </div>
