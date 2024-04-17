cba36da44149b19f6a3b65bc4db6b4e466567363
shivhub09 <shivnagori2020@gmail.com>
2024-04-17 13:27:43 +0530

    main screen added

---
+++ client/src/Components/MainScreen/Components/MainScreen-FirstScreen/MainScreenFirstScreen.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+import "./MainScreenFirstScreen.css"
+const MainScreenFirstScreen = () => {
+    return (
+        <div className="MainScreenFirstScreen">
+            <div className="FirstScreencontainer">
+                <h3 className="mainScreenTitle">WELCOME TO</h3>
+                <h1 className="mainScreenContent">PinIt-Genz</h1>
+            </div>
+
+        </div>
+    )
+}
+
+export default MainScreenFirstScreen
